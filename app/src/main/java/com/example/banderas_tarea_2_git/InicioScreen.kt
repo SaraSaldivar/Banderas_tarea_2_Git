3a42e5acd93f4145be40f142ec01e1034804defa
sara saldivar <sarasaldivar472@gmail.com>
2025-09-24 21:39:51 -0600

    Colores de la bandera de argentina

--- app/src/main/java/com/example/banderas_tarea_2_git/InicioScreen.kt
+++ app/src/main/java/com/example/banderas_tarea_2_git/InicioScreen.kt
@@ -1,2 +1,39 @@
 package com.example.banderas_tarea_2_git
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+
+
+@Composable
+fun Blue(modifier: Modifier = Modifier){
+    Box(
+        modifier = modifier.background(Color.Blue).fillMaxWidth()
+    )
+}
+@Composable
+fun White(modifier: Modifier = Modifier){
+    Box(
+        modifier = modifier.background(Color.White).fillMaxWidth()
+    )
+}
+@Composable
+fun Blue2(modifier: Modifier= Modifier){
+    Box(
+        modifier= modifier.background(Color.Blue).fillMaxWidth()
+    )
+}
+
--- app/src/main/java/com/example/banderas_tarea_2_git/MainActivity.kt
+++ app/src/main/java/com/example/banderas_tarea_2_git/MainActivity.kt
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Banderas_tarea_2_GitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Banderas_tarea_2_GitTheme {
-        Greeting("Android")
-    }
-}
