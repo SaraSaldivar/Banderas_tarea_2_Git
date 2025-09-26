f65bfe26f93097478c8cec173286df5351d0a6c4
sara saldivar <sarasaldivar472@gmail.com>
2025-09-25 22:32:57 -0600

    cambié el height por un dimension.fillToConstraints pq se veía bn feo en el teléfono

--- app/src/main/java/com/example/banderas_tarea_2_git/InicioScreen.kt
+++ app/src/main/java/com/example/banderas_tarea_2_git/InicioScreen.kt
@@ -28,15 +28,19 @@ val rojoPatriaraah = Color(0xFFC81016)
 fun BanderaMexicoConstraint(){
     ConstraintLayout (Modifier.fillMaxSize()){
         val(boxGreen, boxWhite, boxRed)=createRefs()
-        Box(modifier = Modifier.fillMaxWidth().height(290.dp).background(VerdeRaaah).constrainAs(boxGreen){
+        Box(modifier = Modifier.fillMaxWidth().background(VerdeRaaah).constrainAs(boxGreen){
             top.linkTo(parent.top)
+            bottom.linkTo(boxWhite.top)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
+            height = Dimension.fillToConstraints
         })
-        Box(modifier = Modifier.fillMaxWidth().height(290.dp).background(Color.White).constrainAs(boxWhite){
+        Box(modifier = Modifier.fillMaxWidth().background(Color.White).constrainAs(boxWhite){
             top.linkTo(boxGreen.bottom)
+            bottom.linkTo(boxRed.top)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
+            height = Dimension.fillToConstraints
         }){
             ConstraintLayout (modifier = Modifier.fillMaxSize()){
             val (escudo)=createRefs()
@@ -56,10 +60,12 @@ fun BanderaMexicoConstraint(){
         }
 
 
-        Box(modifier = Modifier.fillMaxWidth().height(290.dp).background(rojoPatriaraah).constrainAs(boxRed){
+        Box(modifier = Modifier.fillMaxWidth().background(rojoPatriaraah).constrainAs(boxRed){
+            top.linkTo(boxWhite.bottom)
             bottom.linkTo(parent.bottom)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
+            height = Dimension.fillToConstraints
         })
     }
 }
