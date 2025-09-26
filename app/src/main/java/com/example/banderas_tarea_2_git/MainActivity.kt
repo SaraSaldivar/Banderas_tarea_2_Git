8bd8c86edf3709bde8a8fcd01e42ef7e9c7bffbb
sara saldivar <sarasaldivar472@gmail.com>
2025-09-25 22:18:49 -0600

    la mandamos a llamar en el main, ya andaba corriendo la app y ni la tenía ahí xddd

--- app/src/main/java/com/example/banderas_tarea_2_git/MainActivity.kt
+++ app/src/main/java/com/example/banderas_tarea_2_git/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.banderas_tarea_2_git.ui.theme.Banderas_tarea_2_GitTheme
@@ -20,28 +23,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             Banderas_tarea_2_GitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ){
+                        BanderaMexicoConstraint(
+
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Banderas_tarea_2_GitTheme {
-        Greeting("Android")
-    }
-}
